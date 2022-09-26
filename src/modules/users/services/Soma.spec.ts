4b67111e62477d2933777ad0a886cea3173c0ba0
Henrique Duran <henriqueduran15@gmail.com>
2022-09-26 15:21:20 -0300

    :sparkles: feat: simple sum test

---
+++ src/modules/users/services/Soma.spec.ts
@@ -0,0 +1,15 @@
+describe("Criar categoria", () => {
+  it("Espero que 2+2 seja 4", () => {
+    const sum = 2 + 2;
+    const res = 4;
+
+    expect(sum).toBe(res);
+  });
+
+  it("Espero que 2+2 não seja 5", () => {
+    const soma = 2 + 2;
+    const naoquerer = 5;
+
+    expect(soma).not.toBe(naoquerer);
+  });
+});
