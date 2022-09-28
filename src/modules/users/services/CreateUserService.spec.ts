4f0a73e78de58ca50e96a3f28775a010aa3ca809
Henrique Duran <henriqueduran15@gmail.com>
2022-09-28 03:33:02 -0300

    teste123

--- src/modules/users/services/CreateUserService.spec.ts
+++ src/modules/users/services/CreateUserService.spec.ts
@@ -1,9 +1,35 @@
-import CreateUserService from "./CreateUserService"
+import UsersRepositoryInMemory from "../repositories/in-memory/UsersRepositoryInMemory";
+import CreateUserService from "./CreateUserService";
+import BCryptHashProvider from "@shared/container/providers/HashProvider/implementations/BCryptHashProvider";
+import mailConfig from "@config/mail";
+
+import EtherealMailProvider from "@shared/container/providers/MailProvider/implementations/EtherealMailProvider";
+import SESMailProvider from "../../../shared/container/providers/MailProvider/implementations/SESMailProvider";
+import { container } from "tsyringe";
+
+const mailProviders = {
+  ethereal: container.resolve(EtherealMailProvider),
+  ses: container.resolve(SESMailProvider),
+};
+
+const providers = {
+  bcrypt: BCryptHashProvider,
+};
 
 describe("Create User", () => {
+  let createUserService: CreateUserService;
+  let usersRepositoryInMemory: UsersRepositoryInMemory;
+
+  beforeEach(() => {
+    usersRepositoryInMemory = new UsersRepositoryInMemory();
+    createUserService = new CreateUserService(
+      usersRepositoryInMemory,
+      new providers.bcrypt(),
+      mailProviders[mailConfig.driver]
+    );
+  });
 
   it("should be able to create a user", () => {
 
-    const createUser = new CreateUserService();
-  })
-})
+  });
+});
